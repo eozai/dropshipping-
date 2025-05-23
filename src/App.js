38d9e59dd9bb1112fdda74514bf61cad7f28ae12
eozai <jagadishsuthar27daggh@gmail.com>
2025-05-23 15:05:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", paddingTop: "50px" }}>
+      <h1>Welcome to Your Dropshipping Store!</h1>
+      <p>Start adding products and build your brand.</p>
+    </div>
+  );
+}
+
+export default App;
